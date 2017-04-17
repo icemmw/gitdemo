cbc7eba332af68e4c0f17f8c3995a26ef97acaf7
icemmw <cjw2518915@163.com>
2017-04-17 11:27:25 +0800

    今天创建了皮肤相关

---
+++ ChangeSkin/ChangeSkin/UIImage+HAHA.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+HAHA.h
+//  ChangeSkin
+//
+//  Created by iceblue on 2017/4/11.
+//  Copyright © 2017年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (HAHA)
+
+@end
---
+++ ChangeSkin/ChangeSkin/UIImage+HAHA.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+HAHA.m
+//  ChangeSkin
+//
+//  Created by iceblue on 2017/4/11.
+//  Copyright © 2017年 jiangsu. All rights reserved.
+//
+
+#import "UIImage+HAHA.h"
+
+@implementation UIImage (HAHA)
+
+@end
